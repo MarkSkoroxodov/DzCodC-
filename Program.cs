f08fa6dc98825c54aeb0a5392a32992b915e6756
Mark <markskoroxodov228@mail.ru>
2023-02-28 01:45:15 +0300

    Сделал дз

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿/*namespace main
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Введите первое число:");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
+
+        }
+
+    }
+
+}
+*/
+////////////////////////////////
+///////////////////////////////
+// namespace main
+
+// {
+
+//     class Program
+
+//     {
+
+//         static void Main(string[] args)
+
+//         {
+
+//             Console.WriteLine("Введите первое число:");
+
+//             int a = Convert.ToInt32(Console.ReadLine());
+
+//             Console.WriteLine("Введите второе число:");
+
+//             int b = Convert.ToInt32(Console.ReadLine());
+
+//             Console.WriteLine("Введите третье число:");
+
+//             int c = Convert.ToInt32(Console.ReadLine());
+//             if (a > b)
+//             {
+//                 if (a > c)
+//                 {
+//                     Console.WriteLine("Максимальное число из трех: " + a);
+
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine("Максимальное число из трех: " + c);
+//                 }
+//             }
+//             else if (b > c)
+//             {
+//                 Console.WriteLine("Максимальное число из трех: " + b);
+
+//             }
+//             else
+//             {
+//                 Console.WriteLine("Максимальное число из трех: " + c);
+//             }
+
+//         }
+
+//     }
+// }
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+// Console.Clear();
+
+// int a;
+// Console.WriteLine("Введите число: ");
+// a = Console.Read();
+// if (a%2==0){
+//     Console.WriteLine("Четное ");
+// }
+// else {
+//     Console.WriteLine("Нечетное ");
+// }
+////////////////////////////////////////////
+///////////////////////////////////////////
+// Console.Clear();
+
+// int i = 1;
+// bool not = true;
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Чётные числа от 1 до " + num);
+// while (i <= num)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.Write(i + ", ");
+//         not = false;
+//     }
+//     i++;
+// }
